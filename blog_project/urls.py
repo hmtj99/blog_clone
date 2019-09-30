e899d2b3c0cb3c0f9d9e916e4a02b5dd90188c62
Hemant Jain <hmtj99@gmail.com>
2019-09-30 18:18:10 +0530

    added navbar to base.html

--- blog_project/urls.py
+++ blog_project/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
     path('accounts/login/', views.LoginView.as_view(), name='login'),
-    path('accounts/logout', views.LogoutView.as_view(next_page='/'), name='logout',),
+    path('accounts/logout', views.LogoutView.as_view(next_page='/'), name='logout'),
 
 ]
